784f6e786aec4bc0a6d9a895e8ca4dcd0a5d84a9
dezzyboy <dezzyboy@tradebit.ca>
2021-04-30 00:47:22 -0400

    add cors

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from .routes import *
+from flask_cors import CORS
 
 
 api_bp = Blueprint('api', __name__)
@@ -22,6 +23,7 @@ api.add_resource(Transactions, '/get-transactions')
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app)
     app.register_blueprint(api_bp)
     return app
     
